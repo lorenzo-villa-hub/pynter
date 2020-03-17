c2f6fbbc3d2be946d1f166d0c5148528345ad027
Lorenzo Villa <lorenzo.villa.1994@gmail.com>
2020-03-18 00:28:41 +0100

    Added "get_chempots_boundary"

--- my_functions/phase_diagram/analysis.py
+++ my_functions/phase_diagram/analysis.py
@@ -1,4 +1,5 @@
 
+import numpy as np
 from pymatgen.core.composition import Composition
 from pymatgen.analysis.phase_diagram import PDEntry, PhaseDiagram
 
@@ -28,22 +29,15 @@ class ChempotAnalysis:
     def chempots_reference(self):
         return self._chempots_reference
     
-    
-    # def _get_chempots_reference(self):
-    #     # gets reference chempots from e.p.a of elemental compounds in self.computed_phases
-    #     chempots_ref = {}
-    #     for comp in self.computed_phases:
-    #             if comp.is_element:
-    #                 chempots_ref[comp.elements[0]] = self.computed_phases[comp]/comp.num_atoms            
-    #     return chempots_ref
-    
 
     def _get_chempots_reference(self):
         # gets elemental reference compounds and respective e.p.a with Pymatgen el_ref attribute in PhaseDiagram class
+        # the difference with my version is that this considers having more than 1 calculation and takes the minimum e.p.a
         chempots_ref = {}
         pd = PhaseDiagram(self.get_pd_entries())
         for el in pd.el_refs:
             chempots_ref[el] = pd.el_refs[el].energy_per_atom
+        chempots_ref = {k: v for k, v in sorted(chempots_ref.items(), key=lambda item: item[0])}
         return chempots_ref
    
     def get_pd_entries(self):
@@ -80,8 +74,68 @@ class ChempotAnalysis:
         return chempots_delta
                     
             
-  #  def get_chempots_limit(self,comp1,comp2):
+    def get_chempots_boundary(self,comp1,comp2,fixed_chempot):
+        """
+        Given a fixed chemical potential, gets the values of the remaining two chemical potentials
+        in the boundary between two phases (region where the two phases coexist). Only works for 3-component PD (to check).
+        Given a phase P1 (formula AxByOz) and a phase P2 (formula AiBjOk) the chemical potentials have to satisfy the conditions:
+            form_energy(P1) = x*mu(A) + y*mu(B) +z*mu(O)
+            from_energy(P2) = i*mu(A) + j*mu(B) +k*mu(O)
+        From these conditions the values of mu(A) and mu(B) are determined given a fixed value of mu(O).
+        All of the chemical potentials used here are delta_mu, i.e. relative to the elemental phase(delta_mu(O) = mu(O) - mu_ref(O))
+
+        Parameters
+        ----------
+        comp1,comp2 : (Pymatgen Composition object)
+            Compositions of the two phases at the boundary.
+        fixed_chempot : (Dict)
+            Dictionary with fixed Element as key and respective chemical potential as value ({Element:chempot}).
+
+        Returns
+        -------
+        chempots_boundary : (Dict)
+            Dictionary of chemical potentials.
+        """
+        chempots_boundary ={}
+        for el,chempot in fixed_chempot.items():
+            el_fixed, mu_fixed = el, chempot
+        pd = PhaseDiagram(self.get_pd_entries())
+        for e in pd.all_entries:
+            if e.composition == comp1:
+                entry1=e
+            if e.composition == comp2:
+                entry2=e    
+        e1 = pd.get_form_energy(entry1)
+        e2 = pd.get_form_energy(entry2)
         
+        coeff1 = []
+        coeff2 = []
+        # order of variables (mu) will follow the order of self.chempots_reference which is alphabetically ordered
+        for el in self.chempots_reference:
+            if el != el_fixed:
+                if el not in comp1:
+                    coeff1.append(0)
+                else:
+                    coeff1.append(comp1[el])
+                if el not in comp2:
+                    coeff2.append(0)
+                else:
+                    coeff2.append(comp2[el])                
+        a = np.array([coeff1,coeff2])
+        
+        const1 = e1 - comp1[el_fixed]*mu_fixed if el_fixed in comp1 else e1 
+        const2 = e2 - comp2[el_fixed]*mu_fixed if el_fixed in comp2 else e2
+        b = np.array([const1,const2])
+        
+        x = np.linalg.solve(a, b)
+        # output will follow the order given in input
+        counter = 0
+        for el in self.chempots_reference:
+            if el != el_fixed:
+                chempots_boundary[el] = x[counter]
+                counter += 1
+                
+        return chempots_boundary
                   
                     
         
