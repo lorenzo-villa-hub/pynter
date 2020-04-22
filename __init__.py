1705258c9cb1026ecb14f33e5d0e883c19a1adf3
Lorenzo Villa <lorenzo.villa.1994@gmail.com>
2020-04-22 17:59:41 +0200

    Added yaml Loader to remove warning

--- __init__.py
+++ __init__.py
@@ -1,6 +1,10 @@
+
 import yaml
+import os
 
+homedir = os.getenv("HOME")
+cfgfile = os.path.join(homedir,'pynter','config.yml')
 
-def load_config(cfgfile="config.yml"):
+def load_config(cfgfile=cfgfile):
     with open(cfgfile,"r") as ymlfile:
-        return yaml.load(ymlfile)
+        return yaml.load(ymlfile,Loader=yaml.FullLoader) # add Loader to not get warning
