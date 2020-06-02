7be16ee9a0ea1ed36e7eda37367583e01ed744ce
Lorenzo Villa <lorenzo.villa.1994@gmail.com>
2020-06-02 12:49:29 +0200

    Modified frac charge plot (horizontal line)

--- vasp/analysis.py
+++ vasp/analysis.py
@@ -37,18 +37,22 @@ class JobsAnalysis:
             n = -1*np.around(j.charge(),decimals=1) # express in terms of occupation
             energy_dict[n] = j.final_energy()            
         energy_dict = {k: v for k, v in sorted(energy_dict.items(), key=lambda item: item[0])} #order by charge
+
+        def linear(x,E0,E1):
+            return (E1-E0)*x + E0
         
         for n in energy_dict:
-            E = energy_dict[n]
-            if n == 0:
-                E0 = E
-            if n == 1:
-                E1 = E
+            e = float(energy_dict[n])
+            e0 = float(energy_dict[0])
+            e1 = float(energy_dict[1])
             charges.append(n)
-            energies.append(E0-E)
-        
-        plt.plot(charges,energies,label=self.ds.name)
-        plt.plot([0,1],[0,E0-E1],label='Ideal')
+            e_rescaled = e - linear(n, e0, e1) 
+            energies.append(e_rescaled)
+
+        plt.plot(charges,energies,'o--',label=self.ds.name)
+        plt.hlines(0,xmin=0,xmax=1,linestyles='dashed',label='Exact',color='k')
+        width = max([abs(e) for e in energies])
+        plt.ylim(-3*width,+3*width)
         plt.xlabel('Fractional charge')
         plt.ylabel('Energy (eV)')
         plt.legend()
