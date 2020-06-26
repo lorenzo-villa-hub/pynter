c18b224b4fb71d8c4c602426e872c184d77cab44
Lorenzo Villa <lorenzo.villa.1994@gmail.com>
2020-06-26 15:01:50 +0200

    Added format_compositions

---
+++ tools/format.py
@@ -0,0 +1,34 @@
+
+
+
+
+def format_compositions(string,all_math=False):
+    new_string = []
+    if all_math:
+        new_string.append('$')
+    sp = list(string)
+    for c in sp:
+        if c.isdigit():
+            if sp.index(c) != len(sp)-1:
+                nextc = sp[sp.index(c)+1] 
+                if nextc.isdigit():
+                    newc = c + nextc
+                    skip = True
+                else:
+                    newc = c
+                    skip = False
+            else:
+                newc = c
+                skip = False
+            if not skip:
+                if all_math:
+                    new_string.append('_{%s}'%newc)
+                else:
+                    new_string.append('$_{%s}$'%newc)
+        else:
+            new_string.append(c)
+    if all_math:
+        new_string.append('$')       
+    new_string = ''.join(new_string)
+    
+    return new_string
