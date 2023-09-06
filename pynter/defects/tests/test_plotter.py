fee68350eee7bcfc8366811bb02278426b829f36
Lorenzo Villa <lorenzo.villa.1994@gmail.com>
2023-09-06 17:53:44 +0200

    update tests

--- pynter/defects/tests/test_plotter.py
+++ pynter/defects/tests/test_plotter.py
@@ -10,17 +10,20 @@ import matplotlib
 matplotlib.use('Agg') # no graphical output
 
 from pynter.defects.thermodynamics import ThermoData
-from pynter.defects.plotter import PressurePlotter
+from pynter.defects.plotter import ThermodynamicsPlotter
 
 from pynter.testing.core import PynterTest
+from pynter.defects.tests.test_thermodynamics import TestDefectThermodynamics
 
 
-class TestPressurePlotter(PynterTest):
+class TestThermodynamicsPlotter(PynterTest):
 
     # make sure the methods are callable and do not fail
     def test_pressure_plotter(self):
-        thermo = ThermoData.from_json(self.get_testfile_path('thermodata_NN_1300K.json'))
-        plotter = PressurePlotter()
-        plotter.plot_concentrations(thermo)
-        plotter.plot_concentrations(thermo,output='all',charges=[0,1])
-        plotter.plot_fermi_level(thermo.partial_pressures,thermo.fermi_levels,band_gap=2.48)    
+        test_df_thermo = TestDefectThermodynamics()
+        test_df_thermo.setUp()
+        thermo = test_df_thermo.dt.get_pO2_thermodata(test_df_thermo.pres,temperature=1300)
+        plotter = ThermodynamicsPlotter()
+        plotter.plot_pO2_vs_concentrations(thermo)
+        plotter.plot_pO2_vs_concentrations(thermo,output='all',charges=[0,1])
+        plotter.plot_pO2_vs_fermi_level(thermo.partial_pressures,thermo.fermi_levels,band_gap=2.48)    
--- pynter/defects/tests/test_thermodynamics.py
+++ pynter/defects/tests/test_thermodynamics.py
@@ -11,7 +11,7 @@ import numpy as np
 from pymatgen.electronic_structure.dos import CompleteDos
 
 from pynter.defects.analysis import DefectsAnalysis
-from pynter.defects.thermodynamics import PressureAnalysis, ThermoData, Conductivity
+from pynter.defects.thermodynamics import DefectThermodynamics, ThermoData, Conductivity
 from pynter.phase_diagram.chempots import PressureReservoirs
 from pynter.tools.utils import get_object_from_json
 
@@ -53,24 +53,53 @@ class TestConductivity(PynterTest):
         
         
         
-class TestPressureAnalysis(PynterTest):
+class TestDefectThermodynamics(PynterTest):
     
     def setUp(self):
         self.da = DefectsAnalysis.from_json(self.get_testfile_path('DA_NN_vacancies.json'))
         self.pres = PressureReservoirs.from_json(self.get_testfile_path('pressure_reservoirs_NN.json'))
         self.dos = get_object_from_json(CompleteDos, self.get_testfile_path('Si_DOS.json')) 
-        self.pa = PressureAnalysis(defects_analysis=self.da, bulk_dos=self.dos)
-        self.thermo_test = ThermoData.from_json(self.get_testfile_path('thermodata_NN_1300K.json'))
+        self.dt = DefectThermodynamics(defects_analysis=self.da, bulk_dos=self.dos)
         
-    def test_get_concentrations(self):
-        thermo = self.pa.get_concentrations(self.pres,temperature=1300,name='NN-test')
-        self.assert_object_almost_equal( thermo.as_dict() , self.thermo_test.as_dict(), rtol=1e-03 )
+    def test_get_pO2_thermodata(self):
+        thermo = self.dt.get_pO2_thermodata(self.pres,temperature=1300)
+        VO_test = [
+         2.0897186554078046e+21,
+         3.842304112336487e+19,
+         2.4574284983410856e+19,
+         2.3163394651917566e+19,
+         7.795528260006108e+18
+         ]
+        VO = [df.total['Vac_O'] for df in thermo.defect_concentrations]
+        self.assert_all_close(VO,VO_test,rtol=1e-03)
+        
+        VNa_test = [
+         3.0476730368186044e+19,
+         5.014664143479033e+19,
+         4.882029109309772e+19,
+         4.794075698419085e+19,
+         8.355907668941178e+19
+         ]
+        VNa = [df.total['Vac_Na'] for df in thermo.defect_concentrations]
+        self.assert_all_close(VNa,VNa_test,rtol=1e-03)
+        
+        VNb_test = [
+         0.00036445299521587783,
+         0.00023108995228321433,
+         0.0002687268424849486,
+         0.0007032822425733467,
+         2.4738404252211725
+         ]
+        VNb = [df.total['Vac_Nb'] for df in thermo.defect_concentrations]
+        self.assert_all_close(VNb,VNb_test,rtol=1e-03)        
+        
         
     def test_get_conductivities(self):
         tc = TestConductivity()
         tc.setUp()
         mobilities = tc.conductivity.mobilities
-        thermo_cond = self.pa.get_conductivities(self.pres,mobilities,1300,'NN-sigma-test')
+        thermo = self.dt.get_pO2_thermodata(self.pres,temperature=1300,name='NN-test')
+        conductivities = self.dt.get_conductivities_from_thermodata(thermo, mobilities)
         conductivities_test = [
          21.975733745739,
          0.3064336399157439,
@@ -78,14 +107,11 @@ class TestPressureAnalysis(PynterTest):
          0.018329168707032613,
          0.7672079995432416
          ]
-        self.assert_all_close( thermo_cond.conductivities , conductivities_test )
+        self.assert_all_close(conductivities , conductivities_test )
         
-    def test_get_fermi_levels(self):
-        thermo = self.pa.get_fermi_levels(self.pres,1300,'NN-test')
-        self.assert_all_close( thermo.fermi_levels , self.thermo_test.fermi_levels )
         
-    def test_get_quenched_fermi_levels(self):
-        thermoq = self.pa.get_quenched_fermi_levels(self.pres, 1300, 300)
+    def test_get_pO2_quenched_thermodata(self):
+        thermoq = self.dt.get_pO2_quenched_thermodata(self.pres, 1300, 300)
         fermi_levels = [
          2.4381471172332763,
          1.9536618465423583,
@@ -95,7 +121,7 @@ class TestPressureAnalysis(PynterTest):
          ]
         self.assert_all_close( thermoq.fermi_levels , fermi_levels )
         
-        thermoq = self.pa.get_quenched_fermi_levels(self.pres,1300,300,quenched_species=['Vac_Na'])
+        thermoq = self.dt.get_pO2_quenched_thermodata(self.pres,1300,300,quenched_species=['Vac_Na'])
         fermi_levels = [
          2.096203606796265,
          1.5317770740509031,
@@ -105,7 +131,7 @@ class TestPressureAnalysis(PynterTest):
          ]
         self.assert_all_close( thermoq.fermi_levels , fermi_levels )
         
-        thermoq = self.pa.get_quenched_fermi_levels(self.pres,1300,300,quenched_species=['Vac_O'])
+        thermoq = self.dt.get_pO2_quenched_thermodata(self.pres,1300,300,quenched_species=['Vac_O'])
         fermi_levels = [
          2.454942788314819,
          2.33268869972229,
