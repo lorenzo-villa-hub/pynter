28cfa0499050703a3bf755908983c66e6b9e5254
Lorenzo Villa <lorenzo.villa.1994@gmail.com>
2023-09-08 19:46:20 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md") as file:
 
 setup(
     name='pynter-defects',
-    version='1.2.0',
+    version='1.2.1',
     author='Lorenzo Villa',
     description='Tools for atomistic calculations, provides features for point-defect calculations',
     long_description=long_description,
