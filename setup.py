140891317efb4f83215e22dec922a4740e1e68b3
Lorenzo Villa <lorenzo.villa.1994@gmail.com>
2023-11-13 16:22:34 +0100

    add export arg in job settings

--- setup.py
+++ setup.py
@@ -12,7 +12,7 @@ with open("README.md") as file:
 
 setup(
     name='pynter-defects',
-    version='1.3.5',
+    version='1.3.6',
     author='Lorenzo Villa',
     description='Tools for atomistic calculations, provides features for point-defect calculations',
     long_description=long_description,
