952db3e05a410f86162b0176c8a4ce51e8bfc73c
lorenzo-villa-hub <lorenzo.villa.1994@gmail.com>
2025-05-31 23:15:32 +0200

    add Si defects raw files

---
+++ pynter/defects/tests/test_files/Si-defects/Sub_P_on_Si/q1/1-PBE-SCF/job.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+#SBATCH -A special00003
+#SBATCH --job-name=Si-test_Sub_P_on_Si_q1_PBE-rel_1
+#SBATCH --mail-user=villa@mm.tu-darmstadt.de
+#SBATCH --mail-type=ALL
+#SBATCH --nodes=1
+#SBATCH --ntasks-per-node=96
+#SBATCH --cpus-per-task=1
+#SBATCH --output=out.%j
+#SBATCH --error=err.%j
+#SBATCH --time=24:00:00
+#SBATCH --exclusive
+#SBATCH --mem-per-cpu=3500
+
+module purge
+ml intel/2020.4 
+ml intelmpi/2020.4 
+ml fftw/3.3.10 
+
+srun /home/groups/da_mm/codes/vasp/build/vasp.5.4.4/bin/vasp_std
+
+automation_vasp.py --contcar --chgcar --wavecar --error-check --check-kpoints
