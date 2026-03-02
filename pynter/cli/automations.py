a7b24544d289096dbe4a03fa2b19a22c8bd5d06f
Lorenzo Villa <lorenzo.villa.1994@gmail.com>
2026-03-02 18:33:29 +0100

    implement vasp NEB automations, tests missing

--- pynter/cli/automations.py
+++ pynter/cli/automations.py
@@ -8,7 +8,7 @@ Created on Tue May 30 15:30:49 2023
 import os
 
 from pynter import SETTINGS
-from pynter.jobs.vasp.vasp_automations import VaspAutomation
+from pynter.jobs.vasp.vasp_automations import VaspAutomation, VaspNEBAutomation
 from pynter.jobs.datasets import Dataset
 
 def parse_common_args(parser):
@@ -50,26 +50,70 @@ def run_automation_vasp(args):
                                 sort='path')
     path = os.getcwd()
     job_current = ds.select_jobs(path=path)[0]
+    current_index = ds.jobs.index(job_current)
+    job_next = ds.jobs[current_index + 1] if current_index != (len(ds.jobs) -1) else None # check is not last job
     is_converged_electronic, is_converged_ionic = automation.check_convergence(job_current)
     if is_converged_electronic and is_converged_ionic:
-        current_index = ds.jobs.index(job_current)
-        job_next = ds.jobs[current_index + 1]
-        files = []
-        if args.contcar:
-            files.append('CONTCAR')
-        if args.chgcar:
-            files.append('CHGCAR')
-        if args.wavecar:
-            files.append('WAVECAR')
-        automation.copy_files_from_job1_to_job2(
-                                    job1=job_current,
-                                    job2=job_next,
-                                    filenames=files,
-                                    check_kpoints=args.check_kpoints)
-        
-        job_next.run_job(sync=False,write_input=False)
+        if job_next:
+            files = []
+            if args.contcar:
+                files.append('CONTCAR')
+            if args.chgcar:
+                files.append('CHGCAR')
+            if args.wavecar:
+                files.append('WAVECAR')
+            automation.copy_files_from_job1_to_job2(
+                                        job1=job_current,
+                                        job2=job_next,
+                                        filenames=files,
+                                        check_kpoints=args.check_kpoints)
+            job_next.run_job(sync=False,write_input=False)
+        else:
+            automation.status.append('No next jobs found.')
+    
     automation.write_status(path=path)
 
 
 def run_automation_vasp_neb(args):
-    raise NotImplementedError('VASP NEB automation still to be implemented')
+    automation = VaspNEBAutomation()
+    path = os.getcwd()
+    if os.path.basename(os.path.abspath(path)).isdigit():
+        ds_path = '../../'
+    else:
+        ds_path = '../'
+    ds = Dataset.from_directory(
+                                path=ds_path,
+                                job_script_filenames=args.job_script_filename,
+                                sort='path')
+    job_current = ds.select_jobs(path=path)[0]
+    current_index = ds.jobs.index(job_current)
+    job_next = ds.jobs[current_index + 1] if current_index != (len(ds.jobs) -1) else None # check is not last job
+    error_msg = f'Invalid job class: {job_current.jobclass}. Valid classes are VaspJob and VaspNEBJob.'
+    if job_current.is_converged:
+        if job_next:
+            if job_current.jobclass not in ('VaspJob','VaspNEBJob'):
+                raise ValueError(error_msg)
+            if job_current.jobclass == 'VaspJob':
+                if job_next.jobclass == 'VaspJob':
+                    if current_index != 0:
+                        automation.copy_files_from_job1_to_job2(
+                                            job1=job_current,
+                                            job2=job_next,
+                                            filenames=['CHGCAR','WAVECAR'],
+                                            check_kpoints=args.check_kpoints)    
+                elif job_next.jobclass == 'VaspNEBJob':
+                    automation.copy_images_files(
+                                                source_path='../',
+                                                dest_path=job_next.path,
+                                                filenames=['CHGCAR','WAVECAR']) 
+            elif job_current.jobclass == 'VaspNEBJob':
+                automation.copy_images_files(
+                                            source_path=job_current.path,
+                                            dest_path=job_next.path,
+                                            filenames=['CONTCAR','CHGCAR','WAVECAR']) 
+            #job_next.run_job(sync=False,write_input=False)
+            print(f'Run {job_next.path}')
+        else:
+            automation.status.append('No next jobs found.')
+        
+    automation.write_status(path=path)   
